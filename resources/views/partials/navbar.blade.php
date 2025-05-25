0a18a968c82435d153478d89bb6f52c856645368
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-05-25 12:15:13 +0700

    docs: web performance

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -24,7 +24,7 @@
         </a>
 
         {{-- Hamburger Button --}}
-        <button id="mobileMenuToggle" class="lg:hidden focus:outline-none">
+        <button id="mobileMenuToggle" class="lg:hidden focus:outline-none" aria-label="Toggle mobile menu">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M4 6h16M4 12h16M4 18h16" />
