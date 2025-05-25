44fc488d2e52f7542ef12a912907323f231ea079
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-05-25 18:36:22 +0700

    docs: tailwind font bold into Download Template CSV

--- resources/views/admin/koleksi/import.blade.php
+++ resources/views/admin/koleksi/import.blade.php
@@ -11,7 +11,7 @@
         <div class="text-gray-700">
             <p>Impor data secara massal dengan mengunggah file berformat <strong>.csv</strong>. Sebelum mengunggah, pastikan data Anda telah sesuai dengan template kolom yang disediakan.</p>
             <p>Anda dapat mengunduh contoh template CSV melalui tautan di bawah ini:</p>
-            <a href="{{ asset('template/Template CSV Koleksi VoiceLib v1 - Sheet1.csv') }}" class="text-blue-600 underline font-medium mt-2 inline-block">
+            <a href="{{ asset('template/Template CSV Koleksi VoiceLib v1 - Sheet1.csv') }}" class="text-indigo-600 font-bold underline mt-2 inline-block">
                 Download Template CSV
             </a>
         </div>
