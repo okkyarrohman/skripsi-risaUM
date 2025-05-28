6c81d431d0abed9ee0d93467ae5409087e1a5137
okkyarrohman <okkyzee@gmail.com>
2025-05-28 09:36:06 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,31 +24,28 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 // Admin Dashboard Controller
 Route::prefix('admin')->middleware(['auth'])->group(function () {
 
-    // Dashboard
-    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-    Route::get('/guide-admin', [AdminDashboardController::class, 'guideAdmin'])->name('admin.guide');
-    Route::get('/profile', [AdminDashboardController::class, 'profile'])->name('admin.profile');
+  // Dashboard
+  Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+  Route::get('/guide-admin', [AdminDashboardController::class, 'guideAdmin'])->name('admin.guide');
+  Route::get('/profile', [AdminDashboardController::class, 'profile'])->name('admin.profile');
 
-    // Kelola Data Koleksi
-    Route::resource('koleksi', KoleksiController::class)->names('admin.koleksi');
-    Route::get('/show/koleksi/import', [KoleksiController::class, 'showImport'])->name('admin.koleksi.show.import');
-    Route::post('/store/koleksi/import', [KoleksiController::class, 'storeImport'])->name('admin.koleksi.store.import');
+  // Kelola Data Koleksi
+  Route::resource('koleksi', KoleksiController::class)->names('admin.koleksi');
+  Route::get('/show/koleksi/import', [KoleksiController::class, 'showImport'])->name('admin.koleksi.show.import');
+  Route::post('/store/koleksi/import', [KoleksiController::class, 'storeImport'])->name('admin.koleksi.store.import');
 
-    // Data Audio
-    Route::resource('audio', AudioController::class)->names('admin.audio');
-    Route::get('/audio/by-koleksi/create/{collectionId}', [AudioController::class, 'createByKoleksi'])->name('admin.audio.by.koleksi.create');
-    Route::post('/audio/test-tts', [AudioController::class, 'testTTS'])->name('admin.audio.testTTS');
+  // Data Audio
+  Route::resource('audio', AudioController::class)->names('admin.audio');
+  Route::get('/audio/by-koleksi/create/{collectionId}', [AudioController::class, 'createByKoleksi'])->name('admin.audio.by.koleksi.create');
+  Route::post('/audio/test-tts', [AudioController::class, 'testTTS'])->name('admin.audio.testTTS');
 
-    // Data Mahasiswa
-    Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('admin.mahasiswa');
+  // Data Mahasiswa
+  Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('admin.mahasiswa');
 
-    // Permintaan Full Akses
-    Route::get('/permintaan', [PermintaanController::class, 'index'])->name('admin.permintaan');
+  // Permintaan Full Akses
+  Route::get('/permintaan', [PermintaanController::class, 'index'])->name('admin.permintaan');
 
-    Route::get('/hello', function () {
-        return 'Hello world';
-    });
-    Route::get('/hello1', function () {
-        return 'Hello world1';
-    });
+  Route::get('/hello', function () {
+    return 'Hello world';
+  });
 });
