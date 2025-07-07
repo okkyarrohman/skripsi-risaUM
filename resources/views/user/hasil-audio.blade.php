602c8303413d271dc6571ce83f9f0cd0bc6502a6
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-07-08 02:27:39 +0700

    fix: play button add hover:cursor-pointer

--- resources/views/user/hasil-audio.blade.php
+++ resources/views/user/hasil-audio.blade.php
@@ -66,7 +66,7 @@
                         <button 
                             data-audio-id="audio-{{ $index }}" 
                             id="btn-{{ $index }}" 
-                            class="w-13 h-13 rounded-full bg-[#090445] text-white flex items-center justify-center shadow-md transition duration-300 hover:bg-[#090445]"
+                            class="w-13 h-13 hover:cursor-pointer rounded-full bg-[#090445] text-white flex items-center justify-center shadow-md transition duration-300 hover:bg-[#090445]"
                         >
                             <svg id="icon-{{ $index }}" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                                 <path d="M8 5v14l11-7z" /> <!-- Play Icon -->
