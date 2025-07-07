b5bf2f51d0550872ccf56c8d5ab9e2119675937f
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-07-08 01:31:17 +0700

    fix: hover select language

--- resources/views/user/select-language.blade.php
+++ resources/views/user/select-language.blade.php
@@ -7,17 +7,16 @@
     <h1 class="text-center text-4xl font-bold mb-4 text-[#06003F]">Pilih Bahasa Audio</h1>
     <p class="text-center text-lg mb-10 text-[#06003F]">Silahkan pilih bahasa yang diinginkan untuk mendengar audio abstrak</p>
 
-    <div class="flex flex-col sm:flex-row justify-center gap-6">
+    <div class="flex flex-col sm:flex-row justify-center gap-16">
         <a href="{{ route('cari.audio', ['language' => 'id']) }}" 
-            class="w-full sm:w-48 h-48 bg-[#090445] rounded-2xl p-3 flex flex-col items-center justify-center shadow-lg hover:scale-105 transition-transform text-center"
+            class="w-full sm:w-48 h-48 bg-[#090445] rounded-2xl p-3 flex flex-col items-center justify-center shadow-lg hover:scale-110 hover:bg-[#1119B3] transition-transform text-center"
             role="button"
             aria-label="Pilih Bahasa Indonesia">
                 <h2 class="text-lg font-semibold text-white mb-4">Bahasa Indonesia</h2>
                 <img src="{{ asset('images/flag-id.webp') }}" alt="Bendera Indonesia" class="w-14 h-9 mx-auto">
-            </a>
-
-            <a href="{{ route('cari.audio', ['language' => 'en']) }}" 
-            class="w-full sm:w-48 h-48 bg-[#090445] rounded-2xl p-3 flex flex-col items-center justify-center shadow-lg hover:scale-105 transition-transform text-center"
+        </a>
+        <a href="{{ route('cari.audio', ['language' => 'en']) }}" 
+            class="w-full sm:w-48 h-48 bg-[#090445] rounded-2xl p-3 flex flex-col items-center justify-center shadow-lg hover:scale-110 hover:bg-[#1119B3] transition-transform text-center"
             role="button"
             aria-label="Pilih Bahasa Inggris">
                 <h2 class="text-lg font-semibold text-white mb-4">Bahasa Inggris</h2>
