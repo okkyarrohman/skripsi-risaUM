4d6dce39c520c213e5a2e065c31f5b9d7fe1d9ab
daviddvsjrgg <daviddwiyanto79@gmail.com>
2025-07-08 01:06:05 +0700

    feat: random program studi, jurusan CollectionSeeder

--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Arr;
 use App\Models\Collection;
 
 class CollectionSeeder extends Seeder
@@ -13,13 +14,28 @@ class CollectionSeeder extends Seeder
      */
     public function run(): void
     {
+        // Array fakultas dan program studi
+        $fakultasProdi = [
+            'Fakultas Teknik' => ['Teknik Informatika', 'Teknik Elektro', 'Teknik Sipil'],
+            'Fakultas Ekonomi' => ['Manajemen', 'Akuntansi', 'Ekonomi Pembangunan'],
+            'Fakultas Ilmu Sosial' => ['Sosiologi', 'Ilmu Komunikasi', 'Hubungan Internasional'],
+            'Fakultas Hukum' => ['Ilmu Hukum'],
+            'Fakultas Kedokteran' => ['Pendidikan Dokter', 'Kedokteran Gigi'],
+        ];
+
         for ($i = 1; $i <= 1000; $i++) {
+            // Pilih fakultas secara acak
+            $fakultas = Arr::random(array_keys($fakultasProdi));
+
+            // Pilih program studi berdasarkan fakultas yang dipilih
+            $programStudi = Arr::random($fakultasProdi[$fakultas]);
+
             Collection::create([
                 'judul_tugas_akhir' => "Sistem Informasi Perpustakaan Vol. $i",
                 'nama_penulis' => "Ahmad Fauzi $i",
                 'nama_pembimbing' => "Dr. Budi Santoso",
-                'program_studi' => 'Teknik Informatika',
-                'fakultas' => 'Fakultas Teknik',
+                'program_studi' => $programStudi,
+                'fakultas' => $fakultas,
                 'tahun_terbit' => 2023,
                 'abstrak_indo' => "Penelitian ini membahas tentang topik ke-$i ...",
                 'abstrak_eng' => "This research discusses topic number $i ...",
